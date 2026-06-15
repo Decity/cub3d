c5f978c36692fcb0a7ad99a2c600134d5327cdc7
Elie Belle <elie.strangelove@gmail.com>
2026-06-15 12:13:32 +0200

    added style to .h files

--- inc/defs.h
+++ inc/defs.h
@@ -3,53 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   defs.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crabin <crabin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: elie <elie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 19:29:56 by crabin            #+#    #+#             */
-/*   Updated: 2026/06/12 19:30:00 by crabin           ###   ########.fr       */
+/*   Updated: 2026/06/15 11:55:23 by elie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFS_H
 # define DEFS_H
 
-/* General */
-# define SUCCESS 0
-# define FAILURE 1
+/* ================================ General ================================= */
 
-/* Window Params */
-# define WINDOW_W 1280
-# define WINDOW_H 720
+# define SUCCESS			0
+# define FAILURE			1
 
-/* Player config */
-# define ROT_SPEED 0.1
-# define MOV_SPEED 0.15
-# define FOV 0.66
+/* ============================= Window Params ============================== */
 
-/* Colors */
-# define COLOR_GREEN  0x00FF00
+# define WINDOW_W			1280
+# define WINDOW_H			720
 
-/* FPS counter */
-# define ONE_SECOND_IN_MS 1000
+/* ============================= Player Config ============================== */
 
-/* Rendering */
-//# define BITS_PER_PIXEL 32 // experimenting with dynamic value
+# define ROT_SPEED			0.1
+# define MOV_SPEED			0.15
+# define FOV				0.66
 
-/* Map Tiles */
-# define EMPTY_SPACE '0'
-# define WALL '1'
+/* ================================= Colors ================================= */
 
-/* Relative Bearings */
-# define LEFT 0
-# define RIGHT 1
+# define COLOR_GREEN		0x00FF00
 
-/* Math */
-# define EPSILON 1e-6
+/* ============================== FPS Counter =============================== */
 
-/* Raycasting */
-# define HIT 1
-# define NO_HIT 0
-# define SIDE_EASTWEST 0
-# define SIDE_NORTHSOUTH 1
+# define ONE_SECOND_IN_MS	1000
+
+/* =============================== Map Tiles ================================ */
+
+# define EMPTY_SPACE		'0'
+# define WALL				'1'
+
+/* =========================== Relative Bearings ============================ */
+
+# define LEFT				0
+# define RIGHT				1
+
+/* ================================== Math ================================== */
+
+# define EPSILON			1e-6
+
+/* =============================== Raycasting =============================== */
+
+# define HIT				1
+# define NO_HIT				0
+# define SIDE_EASTWEST		0
+# define SIDE_NORTHSOUTH	1
 
 #endif
